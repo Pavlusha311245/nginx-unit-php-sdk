a23903c24af221fbe626dfe3a1a5155b3f0a8bae
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-08-01 12:30:34 +0300

    Add control interface

---
+++ src/Interfaces/ControlInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pavlusha311245\UnitPhpSdk\Interfaces;
+
+interface ControlInterface
+{
+    // TODO: should be implemented
+    /**
+     * Restart an application
+     *
+     * @return bool
+     */
+    public function restartApplication(): bool;
+}
