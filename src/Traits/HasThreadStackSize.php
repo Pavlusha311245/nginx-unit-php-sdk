47498119a918290db91d44e4fc8b01085a2f2790
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-09-08 10:25:22 +0300

    Add trait HasThreadStackSize

---
+++ src/Traits/HasThreadStackSize.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pavlusha311245\UnitPhpSdk\Traits;
+
+trait HasThreadStackSize
+{
+    private int $_thread_stack_size;
+
+    /**
+     * @return int
+     */
+    public function getThreadStackSize(): int
+    {
+        return $this->_thread_stack_size;
+    }
+
+    /**
+     * @param int $thread_stack_size
+     */
+    public function setThreadStackSize(int $thread_stack_size): void
+    {
+        $this->_thread_stack_size = $thread_stack_size;
+    }
+}
