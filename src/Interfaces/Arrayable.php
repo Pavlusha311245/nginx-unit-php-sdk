79f7e2bafac66f174acaddbae191d8f9d7f7d589
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-09-09 10:11:41 +0300

    Add Arrayble interface

---
+++ src/Interfaces/Arrayable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pavlusha311245\UnitPhpSdk\Interfaces;
+
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray(): array;
+}
