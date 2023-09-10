40197d930a70de35f3348fd44b32f169769f5e3e
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-09-10 12:19:43 +0300

    Update Upstream

--- src/Config/Upstream.php
+++ src/Config/Upstream.php
@@ -10,13 +10,17 @@ class Upstream implements UpstreamInterface
 {
     use HasListeners;
 
-    private array $_servers;
+    private array $_servers = [];
 
     public function __construct(
         private readonly string $_name,
-        array                   $data
+        array                   $data = []
     ) {
-        $this->_servers = $data;
+        if (!empty($data)) {
+            if (array_key_exists('servers', $data)) {
+                $this->setServers($data['servers']);
+            }
+        }
     }
 
     /**
@@ -28,10 +32,49 @@ class Upstream implements UpstreamInterface
     }
 
     /**
+     * @param array $servers
+     */
+    private function setServers(array $servers): void
+    {
+        $this->_servers = $servers;
+    }
+
+    /**
+     * @param string $ip
+     * @param int $weight
+     * @return void
+     */
+    public function setServer(string $ip, int $weight = 1): void
+    {
+        if ($weight < 0 || $weight > 1000000) {
+            throw new OutOfRangeException('Weight should be between 0 and 1000000');
+        }
+
+        $this->_servers[$ip] = [
+            'weight' => $weight
+        ];
+    }
+
+    /**
      * @return array
      */
     public function getServers(): array
     {
-        return $this->_servers['servers'];
+        return $this->_servers;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'servers' => $this->getServers()
+        ];
+    }
+
+    /**
+     * @return string|false
+     */
+    public function toJson(): string|false
+    {
+        return json_encode($this->toArray());
     }
 }
