a243e632a9086021dbd87df2bc5899a7bd19f9f5
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-09-13 14:06:55 +0300

    Update PhpTarget

--- src/Config/Application/Targets/PhpTarget.php
+++ src/Config/Application/Targets/PhpTarget.php
@@ -16,12 +16,15 @@ class PhpTarget
      *
      * @var string
      */
-    private string $_script;
+    private string $_script = 'index.php';
 
     public function __construct(array $data)
     {
+        if (array_key_exists('script', $data)) {
+            $this->setScript($data['script']);
+        }
+
         $this->setRoot($data['root']);
-        $this->setScript($data['script']);
     }
 
     /**
