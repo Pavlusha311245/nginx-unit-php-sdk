3aaa7fc58662647331e186a1db7169187072e974
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-12-05 08:53:27 +0300

    Add hasTargets method

--- src/Traits/HasTargets.php
+++ src/Traits/HasTargets.php
@@ -17,8 +17,18 @@ trait HasTargets
     /**
      * @param array $targets
      */
-    public function setTargets(array $targets): void
+    public function setTargets(array $targets): self
     {
         $this->targets = $targets;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasTargets(): bool
+    {
+        return !empty($this->targets);
     }
 }
