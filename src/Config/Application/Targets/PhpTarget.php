159ed3bba61ce32c9840e2b1f316b0d54265d015
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2023-12-05 07:45:19 +0300

    Update Php target class

--- src/Config/Application/Targets/PhpTarget.php
+++ src/Config/Application/Targets/PhpTarget.php
@@ -2,7 +2,9 @@
 
 namespace UnitPhpSdk\Config\Application\Targets;
 
-class PhpTarget
+use UnitPhpSdk\Contracts\Arrayable;
+
+class PhpTarget implements Arrayable
 {
     /**
      * Base directory of the app’s file structure. All URI paths are relative to it.
@@ -13,15 +15,28 @@ class PhpTarget
 
     /**
      * Filename of a root-based PHP script that serves all requests to the app.
+     * If script is set, all requests to the application are handled by the script you specify in this option
      *
      * @var string
      */
     private string $script = 'index.php';
 
+    /**
+     * Filename added to URI paths that point to directories if no script is set
+     * The requests are served according to their URI paths; if they point to directories, index is used.
+     *
+     * @var string
+     */
+    private string $index = 'index.php';
+
     public function __construct(array $data)
     {
         if (array_key_exists('script', $data)) {
             $this->setScript($data['script']);
+        }
+
+        if (array_key_exists('index', $data)) {
+            $this->setIndex($data['index']);
         }
 
         $this->setRoot($data['root']);
@@ -57,5 +72,33 @@ class PhpTarget
     public function setRoot(string $root): void
     {
         $this->root = $root;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndex(): string
+    {
+        return $this->index;
+    }
+
+    /**
+     * @param string $index
+     */
+    public function setIndex(string $index): void
+    {
+        $this->index = $index;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'root' => $this->getRoot(),
+            'script' => $this->getScript(),
+            'index' => $this->getIndex()
+        ];
     }
 }
